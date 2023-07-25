45fff008c119a0fa57c71ed7141ad17c870e8da9
Nezio6102 <88175031+Nezio6102@users.noreply.github.com>
2023-07-25 16:01:28 +0700

    first commit

---
+++ ToDoListProject/Models/ToDo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoListProject.Models;
+
+public partial class ToDo
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int StatusId { get; set; }
+
+    public string? Description { get; set; }
+
+    public DateTime Deadline { get; set; }
+
+    public virtual Status Status { get; set; } = null!;
+
+    public ToDo(string name, int statusId, string? description, DateTime deadline)
+    {
+        Name = name;
+        StatusId = statusId;
+        Description = description;
+        Deadline = deadline;
+    }
+
+    public ToDo(int id, string name, int statusId, string? description, DateTime deadline)
+    {
+        Id = id;
+        Name = name;
+        StatusId = statusId;
+        Description = description;
+        Deadline = deadline;
+    }
+
+    public ToDo()
+    {
+    }
+
+}
